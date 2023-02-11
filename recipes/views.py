3972f4c9c329c35ebd98fefa4c476954f840d0ba
Matheus Canel <matheusxcanel@gmail.com>
2023-02-11 16:49:56 -0300

    http basico

---
+++ recipes/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from  django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('HOME 1')
+
+def sobre(request):
+    return HttpResponse('sobre')
+
+def contato (request):
+    return HttpResponse ('contato')

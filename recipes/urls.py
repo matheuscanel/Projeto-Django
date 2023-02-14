e4ec1c1cfd6a5572b701d8a52fabd664fadc3ffc
Matheus Canel <matheusxcanel@gmail.com>
2023-02-14 14:02:50 -0300

    removido contanto

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from recipes.views import home, sobre, contato
+from recipes.views import home
 
 
 urlpatterns = [
 
     path('', home ), #home
-    path('sobre/', sobre), #/sobre/
-    path('contato/', contato), #/contato/
+   
     
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
-from  django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Matheus'})
 
-def sobre(request):
-    return render (request, 'me-apague/temp.html')
 
-def contato (request):
-    return HttpResponse ('contato')
